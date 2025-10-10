a1b365a109581abf7031ec4ef00d1531d96cd763
JL <j.lopezlopez1004@gmail.com>
2025-10-10 16:49:32 -0600

    Sincronización automática

---
+++ Script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd "/home/jl/Documentos/DOC_706"
+
+# Añade cambios, haz commit y sube
+git add .
+git commit -m "Sincronización automática"
+git push
+
+# Baja los cambios de otros dispositivos
+git pull
