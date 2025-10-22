3925cd42334cf4177921442fe7720a46e6758f18
JLopezore <j.lopezlopez1004@gmail.com>
2025-10-22 11:25:00 -0600

    sync: auto-update desde EndeavourOS

--- INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/App.js
+++ INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
-import Welcome from './components/Welcome/Welcome';
+import ThemeContext from './context/ThemeContext';
+
+// Importar el Layout y las Páginas
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 import TodoList from './components/TodoList/TodoList';
 import UserDirectory from './components/UserDirectory/UserDirectory';
-import ThemeContext from './context/ThemeContext';
-import { useContext } from 'react';
-import Layout from './components/Layout/Layout'; 
-import Home from './components/Home/Home';
-
 
 function App() {
   const { theme } = useContext(ThemeContext);
+
   return (
     <div className={`App ${theme}`}>
-      <Header />
-      <main>
-        <UserDirectory />
-        <TodoList />
-        <Welcome nombre="Desarrollador" />
-        <Welcome nombre="Jorge" />
-      </main>
+      {/* El componente <Routes> envuelve todas las rutas */}
+      <Routes>
+        {/* Esta es una "Ruta de Layout".
+          Todas las rutas anidadas dentro se renderizarán DENTRO del <Outlet /> de Layout.
+        */}
+        <Route path="/" element={<Layout />}>
+
+          {/* Rutas Hijas */}
+          <Route index element={<Home />} />
+          <Route path="tareas" element={<TodoList />} />
+          <Route path="directorio" element={<UserDirectory />} />
+
+          {/* Ruta "Catch-all" para 404 (No encontrado) */}
+          <Route path="*" element={<h2>Página no encontrada</h2>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/components/Header/Header.js
+++ INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import './Header.css';  
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
   return (
     <header className="app-header">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React Logo" width="100" />
-      <h1>Mi Aplicación Modular</h1>
+      <div className="logo-nav">
+        <h1 className="logo">Mi App</h1>
+        <nav>
+          {/* Usamos <Link> en lugar de <a href=""> */}
+          <Link to="/">Inicio</Link>
+          <Link to="/tareas">Tareas</Link>
+          <Link to="/directorio">Directorio</Link>
+        </nav>
+      </div>
       <ThemeSwitcher />  {/* Componente para cambiar el tema */}
     </header>
   );
