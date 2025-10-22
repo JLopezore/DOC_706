a260b05e8eb6d50060e16288a3d97ad71b745591
JLopezore <j.lopezlopez1004@gmail.com>
2025-10-22 11:40:00 -0600

    sync: auto-update desde EndeavourOS

---
+++ INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/components/Err404/Err404.js
@@ -0,0 +1,13 @@
+import React from "react";  
+import './Err404.css';
+
+const Err404 = () => {
+    return (
+        <div className="err404-container">
+            <h2>Error 404: Página No Encontrada</h2>
+            <p>Lo sentimos, la página que buscas no existe.</p>
+        </div>
+    );
+}
+
+export default Err404;
