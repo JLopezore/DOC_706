c405513bc59bfead30a4e80e63476e095467fded
JLopezore <j.lopezlopez1004@gmail.com>
2025-10-23 09:40:00 -0600

    sync: auto-update desde EndeavourOS

--- INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/components/Header/Header.js
+++ INGENIERIA_SOFTWARE_II/1ER_PARCIAL/mi-app-modular/src/components/Header/Header.js
@@ -7,22 +7,22 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="app-header">
+      <div className="logo-container">
+        <img src={"https://logoeps.com/wp-content/uploads/2013/05/snoopy-character-vector.png"} alt="Logo" className="logo-image" />
+        <h1 className="logo">Mi snoopy App</h1>
+
+      </div>
       <div className="logo-nav">
-        
-          <img src={"https://logoeps.com/wp-content/uploads/2013/05/snoopy-character-vector.png"} alt="Logo" className="logo-image" />
-          <h1 className="logo">Mi snoopy App</h1>
-       
-        <div>
           <nav>
             {/* Usamos <Link> en lugar de <a href=""> */}
             <Link to="/">Inicio</Link>
             <Link to="/tareas">Tareas</Link>
             <Link to="/directorio">Directorio</Link>
           </nav>
-        </div>
-        
       </div>
-      <ThemeSwitcher />  {/* Componente para cambiar el tema */}
+      <div className="theme-switcher-container">
+        <ThemeSwitcher />  {/* Componente para cambiar el tema */}
+      </div>
     </header>
   );
 };
