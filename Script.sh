0ad19d54d1107d601f5c589aea6195100b7f448e
JL <j.lopezlopez1004@gmail.com>
2025-11-04 08:46:37 -0600

    Resolviendo UNSIS

--- Script.sh
+++ Script.sh
@@ -3,7 +3,7 @@ cd "/home/jl/Documentos/DOC_706"
 
 # Añade cambios, haz commit y sube
 git add .
-git commit -m "Sincronización automática"
+git commit -m "Sincronización automática en UNSIS"
 git push
 
 # Baja los cambios de otros dispositivos
